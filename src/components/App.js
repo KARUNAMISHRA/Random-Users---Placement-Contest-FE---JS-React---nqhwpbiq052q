965daef420c91ae4ae67d79cf6e030a1ef0e999f
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-09-03 19:06:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Users from "./users";
 
@@ -6,7 +6,13 @@ const App = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+            setLoading(true);
+            fetch('https://randomuser.me/api/?results=10')
+            .then((res)=>res.json())
+            .then((data)=>{setItems(data.results)});
+            setLoading(false);
+    }, []);
 
     return <Users isLoading={isLoading} items={items} />;
 };
